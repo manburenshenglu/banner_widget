fb349d69533b182cab47382c96887425660ad6db
xiejianlong <308576327@qq.com>
2019-09-20 11:49:53 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:banner_widget/widget/banner_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -45,6 +46,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<BannerItem> data = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    BannerItem item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/64/w1024h640/20181024/wBkr-hmuuiyw6863395.jpg''',
+        '''近日，北大全校教师干部大会刚刚召开，63岁的林建华卸任北大校长；原北大党委书记郝平接替林建华，成为新校长。曾在北京任职多年、去年担任山西高院院长的邱水平回到北大，担任党委书记。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学校长林建华（右）与时任北京大学党委书记郝平（左）''');
+    data.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/99/w1024h675/20181024/FGXD-hmuuiyw6863401.jpg''',
+        '''邱水平、郝平、林建华均为“老北大人”，都曾离开北大，又重归北大任职。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学党委书记郝平讲话''');
+    data.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/107/w1024h683/20181024/kZj2-hmuuiyw6863420.jpg''',
+        '''此番卸任的林建华，亦是北大出身，历任重庆大学、浙江大学、北京大学三所“双一流”高校校长。图为2018年5月4日，北京大学举行建校120周年纪念大会，时任北京大学校长林建华讲话。''');
+    data.add(item);
+    item = BannerItem.defaultBannerItem(
+        '''http://n.sinaimg.cn/news/1_img/vcg/2b0c102b/105/w1024h681/20181024/tOiL-hmuuiyw6863462.jpg''',
+        '''书记转任校长的郝平，为十九届中央委员会候补委员。从北大毕业后留校，后离开北大，历任北京外国语大学校长、教育部副部长。2016年12月，时隔11年，郝平再回北大，出任北大党委书记。''');
+    data.add(item);
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -89,8 +113,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            BannerWidget(data),
             Text(
               'You have pushed the button this many times:',
             ),
--- lib/widget/banner_widget.dart
+++ lib/widget/banner_widget.dart
@@ -1,29 +1,260 @@
-import 'package:flutter/widgets.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-///author xiejianlong
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+const MAX_COUNT = 0x7fffffff;
+enum InfoDirect { HORIZONTAL, VERTICAL }
+enum IndicatorStyle {
+  ///circle indicator
+  CIRCLE,
+
+  ///elliptical indicator
+  ELLIPTICAL
+}
+
+///banner click callback
+typedef void OnBannerItemClick(int position, BannerItem bannerItem);
+
+typedef Widget CustomBuild(int position, BannerItem item);
+
+///author xjl
 ///description
 ///date 2019/7/16 17:29
 ///modify
 class BannerWidget extends StatefulWidget {
-  final int duration = 1000;
-  final double height = 80.0;
+  final int duration;
+  final double height;
+  final List<BannerItem> data;
+  final Color selectedColor;
+  final Color unSelectedColor;
+  final double circleRadius;
+
+  ///elliptical indicator width
+  final double ellipticalWidth;
+
+  ///elliptical indicator height
+  final double ellipticalHeight;
+
+  ///elliptical indicator cornerRadius
+  final double cornerRadius;
+  final IndicatorStyle indicatorStyle;
+
+  ///text background color
+  final Color textBackgroundColor;
+  final OnBannerItemClick onBannerItemClick;
+  final CustomBuild build;
+
+  final InfoDirect textInfoDirect;
+
+  BannerWidget(this.data,
+      {this.duration: 2000,
+      this.height: 200.0,
+      this.selectedColor: Colors.red,
+      this.unSelectedColor: Colors.white,
+      this.onBannerItemClick,
+      this.build,
+      this.textBackgroundColor: const Color(0x33000000),
+      this.textInfoDirect: InfoDirect.HORIZONTAL,
+      this.circleRadius: 5.0,
+      this.indicatorStyle: IndicatorStyle.CIRCLE,
+      this.ellipticalWidth: 16.0,
+      this.ellipticalHeight: 8.0,
+      this.cornerRadius: 10.0})
+      : assert(data != null);
 
   @override
   _BannerWidgetState createState() => _BannerWidgetState();
 }
 
 class _BannerWidgetState extends State<BannerWidget> {
+  Timer _timer;
+
+  PageController _pageController;
+
+  ///current selected banner index
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    double current = widget.data.length > 0
+        ? (MAX_COUNT / 2) - ((MAX_COUNT / 2) % widget.data.length)
+        : 0.0;
+    _pageController = PageController(initialPage: current.toInt());
+    start();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Container(
+      height: widget.height,
+      color: Colors.black12,
+      child: Stack(
+        children: <Widget>[
+          getPageView(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: IntrinsicHeight(
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                color: widget.textBackgroundColor,
+                child: getBannerTextInfoWidget(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void onPageChanged(index) {
+    selectedIndex = index % widget.data.length;
+    setState(() {});
+  }
+
+  ///return banners
+  Widget getPageView() {
+    return PageView.builder(
+        itemCount: widget.data.length > 0 ? MAX_COUNT : 0,
+        controller: _pageController,
+        onPageChanged: onPageChanged,
+        itemBuilder: (context, index) {
+          return InkWell(
+              onTap: () {
+                if (widget.onBannerItemClick != null) {
+                  widget.onBannerItemClick(
+                      selectedIndex, widget.data[selectedIndex]);
+                }
+              },
+              child: widget.build == null
+                  ? FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: widget.data[index % widget.data.length].imgPath,
+                      fit: BoxFit.cover)
+                  : widget.build(
+                      index, widget.data[index % widget.data.length]));
+        });
+  }
+
+  Widget getBannerTextInfoWidget() {
+    if (widget.textInfoDirect == InfoDirect.VERTICAL) {
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            getCurrentTextInfoWidget(),
+            IntrinsicWidth(child: Row(children: indicator()))
+          ]);
+    } else {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(flex: 1, child: getCurrentTextInfoWidget()),
+          Expanded(
+              flex: 0,
+              child: Row(
+                  mainAxisSize: MainAxisSize.min, children: indicator()))
+        ],
+      );
+    }
+  }
+
+  List<Widget> indicator() {
+    List<Widget> indicator = [];
+    for (int i = 0; i < widget.data.length; i++) {
+      indicator.add(
+        Container(
+            margin: EdgeInsets.all(3.0),
+            width: widget.indicatorStyle == IndicatorStyle.CIRCLE
+                ? widget.circleRadius
+                : widget.ellipticalWidth,
+            height: widget.indicatorStyle == IndicatorStyle.CIRCLE
+                ? widget.circleRadius
+                : widget.ellipticalHeight,
+            decoration: BoxDecoration(
+                borderRadius: widget.indicatorStyle == IndicatorStyle.CIRCLE
+                    ? null
+                    : BorderRadius.all(Radius.elliptical(
+                        widget.cornerRadius, widget.cornerRadius)),
+                shape: widget.indicatorStyle == IndicatorStyle.CIRCLE
+                    ? BoxShape.circle
+                    : BoxShape.rectangle,
+                color: i == selectedIndex
+                    ? widget.selectedColor
+                    : widget.unSelectedColor)),
+      );
+    }
+    return indicator;
+  }
+
+  Widget getCurrentTextInfoWidget() {
+    return widget.data.length > 0 && selectedIndex < widget.data.length
+        ? widget.data[selectedIndex].textInfoWidget
+        : Text('$selectedIndex');
+  }
+
+  @override
+  void didUpdateWidget(BannerWidget oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  void stop() {
+    _timer?.cancel();
+    _timer = null;
+  }
+
+  void start() {
+    stop();
+    _timer = Timer.periodic(Duration(milliseconds: widget.duration), (timer) {
+      if (widget.data.length > 0 &&
+          _pageController != null &&
+          _pageController.page != null) {
+        _pageController.animateToPage(_pageController.page.toInt() + 1,
+            duration: Duration(milliseconds: 300), curve: Curves.linear);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    stop();
+    super.dispose();
   }
 }
 
 class BannerItem {
   String imgPath;
-  String description;
+  Widget textInfoWidget;
 
-  BannerItem(this.imgPath, {this.description = ''});
+  static BannerItem defaultBannerItem(String image, String text,
+      [maxLines = 1,
+      style = const TextStyle(
+          color: Colors.white,
+          fontSize: 12.0,
+          decoration: TextDecoration.none)]) {
+    BannerItem item = new BannerItem();
+    item.imgPath = image;
+    Text textWidget = new Text(
+      text,
+      softWrap: true,
+      maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
+      style: style,
+    );
+
+    item.textInfoWidget = textWidget;
+
+    return item;
+  }
 }
